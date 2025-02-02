2922c964a8b9c6fffc536223846eb83920fcb06f
Smeaguuul <144273843+Smeaguuul@users.noreply.github.com>
2025-02-02 20:43:17 +0100

    Part #2 Of Previous Commit

--- SelfHostedSuperViser/Model/APIGetter/AdguardHome.cs
+++ SelfHostedSuperViser/Model/APIGetter/AdguardHome.cs
@@ -9,48 +9,30 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using SelfHostedSuperViser.Model.APIGetter.APIGetter;
+using System.Text.Json.Nodes;
 
 namespace SelfHostedSuperViser.Model.APIGetter.AdguardHome
 {
-    public class AdguardHome : WebsiteAPIModel
+    public class AdguardHome : Service
     {
-        public static async Task<List<APIValue>> CallAPIAsync()
+        protected override string GetEndpoint()
         {
-            // Get the path to the solution directory
-            string solutionDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\.."));
+            return "/control/stats";
+        }
 
-            // Combine the solution directory with the file name
-            string filePath = Path.Combine(solutionDirectory, "SelfHostedSuperViser", "APICalls.json");
-            StreamReader reader = new(filePath);
+        protected override Dictionary<string, string> GetHeaders(JsonElement jsonElement)
+        {
+            var password = jsonElement.GetProperty("Password").GetString();
+            var username = jsonElement.GetProperty("UserName").GetString();
 
-            string text = reader.ReadToEnd();
-
-            var jsonObject = JsonDocument.Parse(text).RootElement.GetProperty("AdguardHome");
-
-            var password = jsonObject.GetProperty("Password").GetString();
-            var username = jsonObject.GetProperty("UserName").GetString();
-            var url = jsonObject.GetProperty("BaseUrl").GetString() + "/control/stats";
-
-            var names = new string[]
-            {
-                "num_dns_queries",
-                "num_blocked_filtering",
-                "avg_processing_time",
-            };
             var plainTextBytes = Encoding.UTF8.GetBytes(username + ":" + password);
             var authorization = Convert.ToBase64String(plainTextBytes);
-            var headers = new Header[]
+            Dictionary<string, string> headers = new Dictionary<string, string>
             {
-                new()
-                {
-                    Name = "Authorization",
-                    Value = "Basic " + authorization,
-                }
+                { "Authorization", "Basic " + authorization }
             };
-            var result = await APIGetter.APIGetter.APIGet(url, names, headers);
 
-            result.Add(new APIValue() { Name = "Website", Value = "AdguardHome" });
-            return result;
+            return headers;
         }
     }
 }
